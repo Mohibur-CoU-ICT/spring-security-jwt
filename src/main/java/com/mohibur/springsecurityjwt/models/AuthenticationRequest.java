6fc943675ff45c657ea7f3f4d46647f954b0824f
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-02-23 00:03:50 +0600

    models added

---
+++ src/main/java/com/mohibur/springsecurityjwt/models/AuthenticationRequest.java
@@ -0,0 +1,27 @@
+package com.mohibur.springsecurityjwt.models;
+
+public class AuthenticationRequest {
+    String username;
+    String password;
+
+    public AuthenticationRequest() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
